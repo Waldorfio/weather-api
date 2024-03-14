cfc4330a066689a69000ec570a02e8dc1fe7c8e3
Walid VM <walid.hanifi707@gmail.com>
2024-03-14 21:08:01 +1000

    📦 NEW: implement cors

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/apiRouter'); // Import apiRouter
 
 const app = express();
+const cors = require('cors'); // Enabling cors for api routes
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
